8596b121a94ef6535249316f82fde77ba5fdd53b
mattw89 <matt.woolway@gmail.com>
2021-01-26 14:34:21 +0000

    useless comment

---
+++ complex/complex.py
@@ -0,0 +1,7 @@
+class ComplexNumber:
+    def __init__(self, r=0, i=0):
+        self.real = r#some bad comments
+        self.imag = i#some bad comments
+
+    def get_data(self):
+        print(f'{self.real}+{self.imag}j')
